20922514c90b8dbec4d30275f3c1f98239b02625
19630940 <58252615+fahruraji@users.noreply.github.com>
2023-01-13 19:42:12 +0800

    Pemecahan starter html

--- index.php
+++ index.php
@@ -5,19 +5,19 @@ scratch. This page gets rid of all links and provides the needed markup only.
 -->
 <html lang="en">
 
-<?php include "partial/head.php" ?>
+<?php include "partials/head.php" ?>
 
 <body class="hold-transition sidebar-mini">
 
   <div class="wrapper">
-    <?php include "partial/nav.php" ?>
-    <?php include "partial/sidebar.php" ?>
+    <?php include "partials/nav.php" ?>
+    <?php include "partials/sidebar.php" ?>
     
     <div class="content-wrapper">
-      <?php include "partial/content.php" ?>
+      <?php include "partials/content.php" ?>
     </div>
-    <?php include "partial/control.php" ?>
-    <?php include "partial/footer.php" ?>
+    <?php include "partials/control.php" ?>
+    <?php include "partials/footer.php" ?>
   </div>
 
 </body>
