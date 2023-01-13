74ce48ed74ab1eda31026738c8319418b442a3a2
19630940 <58252615+fahruraji@users.noreply.github.com>
2023-01-13 19:39:59 +0800

    Initial commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<!--
+This is a starter template page. Use this page to start your new project from
+scratch. This page gets rid of all links and provides the needed markup only.
+-->
+<html lang="en">
+
+<?php include "partial/head.php" ?>
+
+<body class="hold-transition sidebar-mini">
+
+  <div class="wrapper">
+    <?php include "partial/nav.php" ?>
+    <?php include "partial/sidebar.php" ?>
+    
+    <div class="content-wrapper">
+      <?php include "partial/content.php" ?>
+    </div>
+    <?php include "partial/control.php" ?>
+    <?php include "partial/footer.php" ?>
+  </div>
+
+</body>
+
+</html>
