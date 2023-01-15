045ff39feedbd28eea97fead6bd5a6d1f9d643d4
19630940 <58252615+fahruraji@users.noreply.github.com>
2023-01-15 21:15:29 +0800

    Membuat perintah hapus data

---
+++ pages/admin/lokasidelete.php
@@ -0,0 +1,20 @@
+<?php
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+
+    $database = new Database();
+    $db = $database->getConnection();
+
+    $deleteSql = "DELETE FROM lokasi WHERE id = ?";
+    $stmt = $db->prepare($deleteSql);
+    $stmt->bindParam(1, $_GET['id']);
+    if ($stmt->execute()) {
+        $_SESSION['hasil'] = true;
+        $_SESSION['pesan'] = "Berhasil simpan data";
+    } else {
+        $_SESSION['hasil'] = false;
+        $_SESSION['pesan'] = "Gagal simpan data";
+    }
+    echo "<meta http-equiv='refresh' content='0;url=?page=lokasiread'>";
+}
+?>
